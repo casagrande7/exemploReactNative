24249caf654377565c1283a0b2a8f4efe68aceff
casagrande7 <joaofootz10@gmail.com>
2024-02-22 10:48:02 -0300

    AT

--- src/login/LoginScreen.tsx
+++ src/login/LoginScreen.tsx
@@ -4,9 +4,9 @@ import { Image, StyleSheet, Text, TextInput, View } from "react-native";
 function LoginScreen(): JSX.Element {
     return (
        <View style= {styles.container}>
-        <Image resizeMode="contain" source={require('../assets/imagens/user.png')}></Image>
+        <Image style= {styles.logo}resizeMode="contain" source={require('../assets/imagens/user.png')}></Image>
         <View style= {styles.card}>
-            <Text>Login</Text>
+            <Text style= {styles.title}>Login</Text>
             <TextInput placeholder="E-mail" placeholderTextColor={"151413"}></TextInput>
             <TextInput placeholder="Senha" placeholderTextColor={"#151413"}></TextInput>
         </View>
@@ -21,6 +21,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#1e90ff'
     },
+    logo: {
+        width: 80,
+        height: 80, 
+        marginBottom: 20,
+        marginTop: -150
+    },
     card: {
         backgroundColor: '#FFFFFF',
         width: 300,
@@ -31,6 +37,10 @@ const styles = StyleSheet.create({
         shadowOffset: {width:0, height:2},
         shadowOpacity: 0.8,
         shadowRadius: 2,
+    },
+    title: {
+        
+
     }
 
 });
