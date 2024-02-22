c7d43d1ff2b0d20e647dd6f3dc2530de4507b1c6
casagrande7 <joaofootz10@gmail.com>
2024-02-22 13:56:12 -0300

    Atualização

--- src/login/LoginScreen.tsx
+++ src/login/LoginScreen.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 function LoginScreen(): JSX.Element {
+
+    const [email, setEmail]= useState("");
+    const [password, setPassword]= useState("");
+
+    function login(){
+        const dados ={
+            email: email,
+            password: password,
+        }
+        console.log(dados);
+    }
     return (
        <View style= {styles.container}>
         <Image style= {styles.logo}resizeMode="contain" source={require('../assets/imagens/user.png')}></Image>
         <View style= {styles.card}>
             <Text style= {styles.title}>Login</Text>
-            <TextInput style={styles.input} placeholder="E-mail" placeholderTextColor={"#151413"}></TextInput>
-            <TextInput style={styles.input} placeholder="Senha" placeholderTextColor={"#151413"}></TextInput>
+            <TextInput style={styles.input} placeholder="E-mail" placeholderTextColor={"#151413"} onChangeText={(textEmail) => setEmail(textEmail)}></TextInput>
+            <TextInput style={styles.input} placeholder="Senha" placeholderTextColor={"#151413"} onChangeText={(textpassword) => setPassword(textpassword)} secureTextEntry></TextInput>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Entrar</Text>
+                <Text style={styles.buttonText} onPress={() => {login()}}>Entrar</Text>
             </TouchableOpacity>
             <TouchableOpacity>
                 <Text style={styles.forgotPassword}>Esqueceu a senha?</Text>
@@ -28,7 +39,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#1e90ff'
+        backgroundColor: '#000080'
     },
     logo: {
         width: 80,
@@ -64,7 +75,7 @@ const styles = StyleSheet.create({
         borderColor: '#1e90ff'
     },
     button: {
-        backgroundColor: '#1e90ff',
+        backgroundColor: '#000080',
         height: 40,
         borderRadius: 12
 
